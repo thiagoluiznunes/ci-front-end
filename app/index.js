4acf70c73083cde6473f0ef5c1bd6cd25782b6d8
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-03-05 18:49:38 -0300

    add angular material dependencies

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('ci-app', [
+  'ngAnimate',
+  'ngAria',
+  'ngMessages',
+  'ngMaterial',
   'ui.router',
   'toastr']
 );
--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -10,6 +10,10 @@ gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts']);
 gulp.task('deps.js', () => {
   return gulp.src([
     'node_modules/angular/angular.min.js',
+    'node_modules/angular-animate/angular-animate.min.js',
+    'node_modules/angular-aria/angular-aria.min.js',
+    'node_modules/angular-messages/angular-messages.min.js',
+    'node_modules/angular-material/angular-material.min.js',
     'node_modules/@uirouter/angularjs/release/angular-ui-router.min.js',
     'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
     'node_modules/jquery/dist/jquery.min.js',
