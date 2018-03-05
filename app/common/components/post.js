cba20cefc7e4fa60d3c397174b35b65f4eb97adf
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-03-04 21:35:29 -0300

    fix html

--- app/common/components/post.js
+++ app/common/components/post.js
@@ -12,16 +12,14 @@
         data: '@'},
       template: `
         <article class="main">
-          <div class="row">
-            <div class="col-10">
-              <h2 class="">{{ $ctrl.type }}</h2>
-              <p>{{ $ctrl.description }}</p>
-              <p id="h2-data">
-                <a href="{{ $ctrl.contact }}">{{ $ctrl.name }}<span>-</span></a>
-                <i class="fa fa-bookmark"></i>
-                  {{ $ctrl.data }}
-              </p>
-            </div>
+          <div class="col-10">
+            <h2 class="">{{ $ctrl.type }}</h2>
+            <p>{{ $ctrl.description }}</p>
+            <p id="h2-data">
+              <a href="{{ $ctrl.contact }}">{{ $ctrl.name }}<span>-</span></a>
+              <i class="fa fa-bookmark"></i>
+                {{ $ctrl.data }}
+            </p>
           </div>
         </article>
         `});
