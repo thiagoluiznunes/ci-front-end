b6f3c5a4fd01a5f6d550376242d2985a08085c56
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-03-06 15:00:17 -0300

    implements checkbox handler

--- app/controllers/postController.js
+++ app/controllers/postController.js
@@ -12,21 +12,22 @@
     const vm = this;
 
     vm.posts = {};
+    vm.found = 'found';
+    vm.lost = 'lost';
     vm.floatButton = document.getElementById('floatButton');
-    vm.check_f = 'found';
-    vm.check_l = 'lost';
+    vm.checkbox_f = document.getElementById('checkbox_f');
+    vm.checkbox_l = document.getElementById('checkbox_l');
 
     vm.addPost = () => {
       $('#myModal').modal('show');
     };
 
     vm.checkbox = (param) => {
-      console.log(param);
-      // if (param === 'found') {
-      //   console.log('oi');
-      // } else {
-      //   console.log('oi 2');
-      // }
+      if (param === 'found') {
+        document.getElementById('checkbox_l').checked = false;
+      } else {
+        document.getElementById('checkbox_f').checked = false;
+      }
     };
 
     vm.get = () => {
