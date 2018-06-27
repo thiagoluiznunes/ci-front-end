4db2bf194dcedfaf135b1da282c495862999e237
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-06-27 02:28:06 -0300

    refactor submit function

--- app/components/login/loginFactoryValidate.js
+++ app/components/login/loginFactoryValidate.js
@@ -9,33 +9,39 @@
     ['$http', 'consts', 'loginFactory'];
 
   function ValidateFactory($http, consts, loginFactory) {
-    let methods = {};
+    const vm = this;
 
-    methods.validateToken = (token, callback) => {
-      if (token) {
-        $http.post(`${consts.oapiUrl}/validateToken`, {token})
-          .then((response) => {
-            if (!response.data.valid) {
-              console.log('Error validate response, logout is called');
-              loginFactory.logout();
-            } else {
-              $http.defaults.headers.common.Authorization =
-                loginFactory.getUser().token;
-              if (callback) callback(null, response.data.valid);
-            }
-          })
-          .catch((response) => {
-            if (callback) callback(response.data);
-          });
-      } else {
-        if (callback) callback('Invalid token!');
-      }
+    vm.methods = {};
+
+    vm.methods.validateToken = (token, callback) => {
+      validateToken(token, callback, $http, consts, loginFactory);
     };
 
-    methods.validateUser = () => {
+    // vm.methods.validateToken = (token, callback) => {
+    //   if (token) {
+    //     $http.post(`${consts.oapiUrl}/validateToken`, {token})
+    //       .then((response) => {
+    //         if (!response.data.valid) {
+    //           console.log('Error validate response, logout is called');
+    //           loginFactory.logout();
+    //         } else {
+    //           $http.defaults.headers.common.Authorization =
+    //             loginFactory.getUser().token;
+    //           if (callback) callback(null, response.data.valid);
+    //         }
+    //       })
+    //       .catch((response) => {
+    //         if (callback) callback(response.data);
+    //       });
+    //   } else {
+    //     if (callback) callback('Invalid token!');
+    //   }
+    // };
+
+    vm.methods.validateUser = () => {
       let user = loginFactory.getUser();
       if (user && !user.isValid) {
-        methods.validateToken(user.token, (err, valid) => {
+        vm.methods.validateToken(user.token, (err, valid) => {
           if (err) {
             console.log('User is not valid');
           } else {
@@ -47,6 +53,27 @@
       }
     };
 
-    return methods;
+    return vm.methods;
+  }
+
+  function validateToken(token, callback, ...params) {
+    if (token) {
+      params[0].post(`${params[1].oapiUrl}/validateToken`, {token})
+        .then((response) => {
+          if (!response.data.valid) {
+            console.log('Error validate response, logout is called');
+            params[2].logout();
+          } else {
+            params[0].defaults.headers.common.Authorization =
+              params[2].getUser().token;
+            if (callback) callback(null, response.data.valid);
+          }
+        })
+        .catch((response) => {
+          if (callback) callback(response.data);
+        });
+    } else {
+      if (callback) callback('Invalid token!');
+    }
   }
 })();
