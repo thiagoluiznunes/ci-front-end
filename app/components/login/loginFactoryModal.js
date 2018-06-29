12ff5c7c4deb22967d173ffa3e80ada0312d778f
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2018-06-29 02:00:09 -0300

    fix forgot injection error

--- app/components/login/loginFactoryModal.js
+++ app/components/login/loginFactoryModal.js
@@ -8,10 +8,25 @@
   function ModalFactory() {
     let methods = {};
 
-    methods.showLogin = () => {
+    methods.showModal = () => {
       $('#loginModal').modal('show');
     };
 
+    methods.setOptions = (vm, option) => {
+      if (option === 'login') {
+        vm.loginMode = true;
+        vm.signupMode = false;
+        vm.forgotMode = false;
+      } else if (option === 'signup') {
+        vm.loginMode = false;
+        vm.signupMode = true;
+        vm.forgotMode = false;
+      } else {
+        vm.loginMode = false;
+        vm.signupMode = false;
+        vm.forgotMode = true;
+      }
+    };
     return methods;
   }
 })();
